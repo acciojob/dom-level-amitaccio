d19d75fdeded5384a033db17f334f96356ec3589
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-27 12:31:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const targetelm= document.getElementById("level");
+if(targetelm){
+	
+	let domlevel = 0;
+	 let parentelm = targetelm.parentNode;
+	
+	 while ( parentelm.tagName !== 'HTML') {
+      domLevel++;
+      alert(domlevel);
+	 }
+	
+}
+
+
+
+
